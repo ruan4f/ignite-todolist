5cb4e74618225496cf5358d2061119b1f1cb97b8
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-02-10 20:59:49 -0300

    Update TodoList.tsx

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,11 +1,38 @@
+import { ChangeEvent, FormEvent, useState } from 'react'
 import styles from './TodoList.module.css'
 
+interface Task {
+  id: string
+  description: string
+}
+
 export function TodoList() {
+  const [newTaskText, setNewTaskText] = useState('')
+  const [tasks, setTasks] = useState<Task[]>()
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault()
+
+    const newTask = {
+      id: '',
+      description: newTaskText,
+    }
+
+    setTasks([...tasks, newTask])
+  }
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLTextAreaElement>) {
+    event.target.setCustomValidity('')
+    setNewTaskText(event.target.value)
+  }
+
   return (
     <main className={styles.container}>
       <div className={styles.newTask}>
-        <input type="text" />
-        <button>Criar</button>
+        <form onSubmit={handleCreateNewTask}>
+          <input type="text" />
+          <button>Criar</button>
+        </form>
       </div>
     </main>
   )
