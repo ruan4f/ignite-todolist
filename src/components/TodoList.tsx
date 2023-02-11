e5198823e9022c711f3b4c9a9f6e76c77d62a9c6
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2023-02-11 11:16:06 -0300

    Update TodoList.tsx

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,14 +1,15 @@
-import { ChangeEvent, FormEvent, useState } from 'react'
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react'
 import styles from './TodoList.module.css'
 
 interface Task {
   id: string
   description: string
+  completed: boolean
 }
 
 export function TodoList() {
   const [newTaskText, setNewTaskText] = useState('')
-  const [tasks, setTasks] = useState<Task[]>()
+  const [tasks, setTasks] = useState<Task[]>([])
 
   function handleCreateNewTask(event: FormEvent) {
     event.preventDefault()
@@ -16,21 +17,33 @@ export function TodoList() {
     const newTask = {
       id: '',
       description: newTaskText,
+      completed: false,
     }
 
     setTasks([...tasks, newTask])
+    setNewTaskText('')
   }
 
-  function handleNewTaskChange(event: ChangeEvent<HTMLTextAreaElement>) {
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
     event.target.setCustomValidity('')
     setNewTaskText(event.target.value)
+  }
+
+  function handleNewTaskInvalid(event: InvalidEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('Esse campo é obrigatório!')
   }
 
   return (
     <main className={styles.container}>
       <div className={styles.newTask}>
         <form onSubmit={handleCreateNewTask}>
-          <input type="text" />
+          <input
+            type="text"
+            value={newTaskText}
+            onChange={handleNewTaskChange}
+            onInvalid={handleNewTaskInvalid}
+            required
+          />
           <button>Criar</button>
         </form>
       </div>
